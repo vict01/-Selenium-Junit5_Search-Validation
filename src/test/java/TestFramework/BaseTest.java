785e4fd84a2a918db0b541ece171f8b16da7cd94
vict01 <victorcaminero86@gmail.com>
2024-10-04 00:51:33 +0200

    Actualizo pom.xml y BaseTest

--- src/test/java/TestFramework/BaseTest.java
+++ src/test/java/TestFramework/BaseTest.java
@@ -2,24 +2,31 @@ package TestFramework;
 
 import org.junit.After;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public abstract class BaseTest {
     public String urlApp = "https://www.statista.com/companies/search";
-    private static WebDriver driver;
+    private WebDriver driver; // Elimina 'static'
 
     public WebDriver getDriver() {
         return driver;
     }
 
-    public static void beforeTest() {
+    @Before
+    public void beforeTest() { // Elimina 'static'
         try {
-            WebDriverManager.chromedriver().setup();
+            WebDriverManager.chromedriver().forceDownload().setup();
             driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
             maximiseWindow();
         } catch (Exception e) {
             e.printStackTrace();
@@ -32,15 +39,14 @@ public abstract class BaseTest {
         closeBrowser();
     }
 
-    protected static void closeBrowser() {
+    protected void closeBrowser() { // Elimina 'static'
         if (null != driver) {
             driver.close();
             driver.quit();
         }
     }
 
-    protected static void maximiseWindow() {
+    protected void maximiseWindow() { // Elimina 'static'
         driver.manage().window().maximize();
     }
-
 }
